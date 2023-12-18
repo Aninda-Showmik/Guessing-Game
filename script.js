a3dd170119f28ab1a0c24ef9c8da6c5689e1dfb7
Aninda Das <139395892+Aninda-Showmik@users.noreply.github.com>
2023-12-19 03:40:18 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function guess_game() {
+  let low = 1;
+  let high = 10;
+
+  let correct_ans = getRandomInt(low, high);
+  console.log(correct_ans);
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  let c = 3;
+  let input = [];
+  for (let i = 0; i < 3; i++) {
+    input[i] = parseInt(prompt(`Guess a number between 1 to 10 (${c} chances left):`));
+
+    if (input[i] < correct_ans) {
+      alert("Correct answer is higher!");
+    } else if (input[i] > correct_ans) {
+      alert("Correct answer is lower!");
+    } else if (input[i] === correct_ans) {
+      alert("Yahoo! You Win!!");
+      displayRestartButton();
+      break;
+    } else {
+      alert("Please input something Valid!!");
+    }
+
+    c = c - 1;
+
+    if (c === 0) {
+      alert("Oh No! You Lose!!");
+      displayRestartButton();
+      return;
+    }
+  }
+}
+function displayRestartButton() {
+  let restartBtn = document.getElementById("restart-btn");
+  restartBtn.style.display = "block";
+}
+
+document.getElementById("restart-btn").addEventListener("click", guess_game)
+
+
+guess_game();
